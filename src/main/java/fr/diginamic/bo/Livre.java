7eedee4de179e0676b4ea0dd6b49de81cd71ee6f
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-07-30 17:32:14 +0200

    feat(Livre.java): "Add Livre entity class with all the fields : id, title, author"

--- src/main/java/fr/diginamic/bo/Livre.java
+++ src/main/java/fr/diginamic/bo/Livre.java
@@ -1,4 +1,52 @@
 package fr.diginamic.bo;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "LIVRE")
 public class Livre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="TITRE")
+    private String titre;
+
+    @Column(name="AUTEUR")
+    private String auteur;
+
+    public Livre() {
+
+    }
+
+    public Livre(String titre, String auteur) {
+        this.titre = titre;
+        this.auteur = auteur;
+    }
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
 }
